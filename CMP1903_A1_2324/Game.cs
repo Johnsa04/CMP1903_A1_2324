8d099309a8e1330b8844a6432b1ea0d8f47bc27e
Johnsa04 <anthony.johns2005@gmail.com>
2024-03-02 01:21:42 +0000

    forgot brackets lol.cs

--- CMP1903_A1_2324/Game.cs
+++ CMP1903_A1_2324/Game.cs
@@ -12,7 +12,7 @@ namespace CMP1903_A1_2324
         Die dice_2 = new Die();
         Die dice_3 = new Die();
 
-        int roll_total = dice_1.dice_roll_value + dice_2.dice_roll_value + dice_3.dice_roll_value
+        int roll_total = dice_1.dice_roll_value() + dice_2.dice_roll_value() + dice_3.dice_roll_value()
         console.writeline("the sum of the dice rolls was ", roll_total )
         /*
          * The Game class should create three die objects, roll them, sum and report the total of the three dice rolls.
