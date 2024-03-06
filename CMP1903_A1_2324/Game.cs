704d715942da7ff0877998ca0528ea58353f26e2
Johnsa04 <158074212+Johnsa04@users.noreply.github.com>
2024-03-06 14:10:21 +0000

    finished game

--- CMP1903_A1_2324/Game.cs
+++ CMP1903_A1_2324/Game.cs
@@ -10,6 +10,24 @@ namespace CMP1903_A1_2324
 {
     internal class Game
     {
-       
+        private Random random = new Random();
+        public Die Dice_1;
+        public Die Dice_2;
+        public Die Dice_3;
+
+        public Game()
+        {
+            Dice_1 = new Die(random);
+            Dice_2 = new Die(random);
+            Dice_3 = new Die(random);
+        }
+
+        public int Play()
+        {
+            int result = Dice_1.Roll() + Dice_2.Roll() + Dice_3.Roll();
+            return result;
+            
+        }
+        
     }
 }
