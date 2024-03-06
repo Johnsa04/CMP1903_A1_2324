b8f24266857f9c173111d3634c0daf24b18298bd
Johnsa04 <158074212+Johnsa04@users.noreply.github.com>
2024-03-06 14:11:29 +0000

    die finished.cs

--- CMP1903_A1_2324/Die.cs
+++ CMP1903_A1_2324/Die.cs
@@ -9,6 +9,20 @@ namespace CMP1903_A1_2324
 {
     internal class Die
     {
-      
+        private Random random;
+
+        public int CurrentValue;
+
+        public Die(Random random)
+        {
+            this.random = random;
+            Roll();
+        }
+
+        public int Roll()
+        {
+            CurrentValue = random.Next(1, 7);
+            return CurrentValue;
+        }
     }
 }
